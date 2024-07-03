53fa7405598bfb357f96b131ef2680ff254d5d54
mirshaduk <119918613+mirshaduk@users.noreply.github.com>
2024-07-03 09:54:37 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x;          //variable name of x type int ---> can store a single integer which means x can  either store 1 or 2  or any single integer.
+
+
+                //Array can be used to store multiple values in a singke variable name   
+
+    int a[5];  //This means it is an arry of namea which can store multiple integers , it can store 6 integers
+                //Note the indexing of arry starts from 0 not 1
+
+                //elements starting from 0-5
+
+                //The first elements will have an index 0 --> a[0]
+
+                //The second element will have an index 1 -->a [1]
+
+                //And goes till a[5]  
+                    
+                    }
+
---
+++ Array1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+    cout<<"Give 5 inyers as input"<<endl;
+
+    cin>>a>>b>>c>>d>>e;
+
+    cout<<"the inters in reverse order are: "<<e<<d<<c<<b<<a;
+
+    return 0;
+
+}
